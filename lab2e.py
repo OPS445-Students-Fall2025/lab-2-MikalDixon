a37d0506bdd802db340168783390f2cafeb981b1
Mikal Dixon <mdixon21@myseneca.ca>
2025-09-16 16:09:01 -0400

    added lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Author: Mikal Dixon
+# Author ID: mdixon21
+# Date Created: 2025/09/16
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
