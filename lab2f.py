378da649a6b56b6222b4ced6493ded5c8c78529a
Mikal Dixon <mdixon21@myseneca.ca>
2025-09-16 16:22:48 -0400

    added lab2f.py

---
+++ lab2f.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Author: Mikal Dixon
+# Author ID: mdixon21
+# Date Created: 2025/09/16
+import sys
+timer = int(sys.argv[1])
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
