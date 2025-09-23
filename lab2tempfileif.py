7c3962e791b0731e360c22ca49caee1b9a1f33f8
Mikal Dixon <mdixon21@myseneca.ca>
2025-09-23 18:49:16 -0400

    My local changes before merge

---
+++ lab2tempfileif.py
@@ -0,0 +1,3 @@
+password = input('Please enter a secret password')
+if password == 'P@ssw0rd':
+    print('You have succesfully used the right password')
---
+++ showargs.py
@@ -0,0 +1,7 @@
+import sys
+arguments = sys.argv
+name = sys.argv[0]
+
+print('Print out ALL script arguments: ', arguments)
+print('Print out the script name: ' + name)
+print('Print out the number of argument: ', len(arguments))
